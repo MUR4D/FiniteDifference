e0f38487d554b165e8028bec48ff6ad7c0243666
MUR4D <61189656+MUR4D@users.noreply.github.com>
2023-12-14 16:25:23 +0400

    Create FiniteDifference.cpp

---
+++ FiniteDifference.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+void FiniteDifference(int N, double h) {
+    double *u = new double[N + 1];
+    double *x = new double[N + 1];
+
+    u[0] = 0.0;
+    u[N] = 1.0;
+
+    for (int i = 0; i <= N; ++i) {
+        x[i] = i * h;
+    }
+
+    for (int i = 1; i < N; ++i) {
+        u[i + 1] = 2 * u[i] - u[i - 1] + h * h * (std::pow(u[i], 3) - std::sin(x[i]));
+    }
+
+    std::cout << "Solution:\n";
+    for (int i = 0; i <= N; ++i) {
+        std::cout << "u(" << x[i] << ") = " << u[i] << std::endl;
+    }
+
+    // Освобождение памяти
+    delete[] u;
+    delete[] x;
+}
+
+int main() {
+    int N = 1000;
+    double h = 0.001; 
+
+    FiniteDifference(N, h);
+
+    return 0;
+}
